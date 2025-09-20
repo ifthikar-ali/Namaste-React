3f41bef34db359506539723b380d881e5bae0e3a
Ifthikar Ali <ifthikaralijp@gmail.com>
2025-09-21 01:13:15 +0900

    feat: component types

--- App.js
+++ App.js
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom/client";
 
 // JSX => Babel transpiles it to React.createElement => ReactElement-JS object => HTMLElement(render)
 const jsxHeading = <h1 id="heading">Hello from JSX.</h1>;
-console.log(jsxHeading);
 
+// React Component
+// Class based Component - OLD
+// Functional Component - NEW
 const reactRoot = ReactDOM.createRoot(document.getElementById("react-root"));
 reactRoot.render(jsxHeading);
